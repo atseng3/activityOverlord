51f4e39415f346a85c64cf437ab92a5621fe3a65
Albert Tseng <atseng3@gmail.com>
2014-01-12 22:34:47 -0800

    Ep5 add error handling and flash message

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -18,7 +18,9 @@
 module.exports = {
     
 	'new': function(req, res) {
+		res.locals.flash = _.clone(req.session.flash);
 		res.view();
+		req.session.flash = {};
 	},
 	
 	'create': function(req, res) {
@@ -26,11 +28,22 @@ module.exports = {
 		// the sign-up form --> new.ejs
 		User.create(req.params.all(), function userCreated(err, user) {
 			// If there's an error
-			if (err) return next(err);
+			// if (err) return next(err);
+			
+			if (err) {
+				console.log(err);
+				req.session.flash = {
+					err: err
+				}
+				
+				// If error redirect back to sign-up page
+				return res.redirect('/user/new');
+			}
 			
 			// After successfully creating the user
 			// redirect to the show action
 			res.json(user);
+			req.session.flash = {};
 		});
 	},
 
